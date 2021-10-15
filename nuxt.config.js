e96169cf6f4a005e4dcdddebda2396aecfe9e4c0
Chris <chirssel@gmail.com>
2021-10-15 19:31:44 +0200

    Add Nuxt application template

---
+++ nuxt.config.js
@@ -0,0 +1,24 @@
+export default {
+    head: {
+        title: 'not-hacker-news',
+        htmlAttrs: {
+            lang: 'en',
+        },
+        meta: [
+            { charset: 'utf-8' },
+            {
+                name: 'viewport',
+                content: 'width=device-width, initial-scale=1',
+            },
+            { hid: 'description', name: 'description', content: '' },
+            { name: 'format-detection', content: 'telephone=no' },
+        ],
+        link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    },
+    css: [],
+    plugins: [],
+    components: true,
+    buildModules: ['@nuxt/typescript-build', 'nuxt-windicss'],
+    modules: [],
+    build: {},
+};
