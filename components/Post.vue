7ad365faf3d3a11fdf0f1aa19cf878bf1a787b1d
Chris <chirssel@gmail.com>
2021-10-15 21:10:05 +0200

    Design and implement the post list

---
+++ components/Post.vue
@@ -0,0 +1,84 @@
+<template>
+    <div class="flex items-center gap-4">
+        <p class="w-8 text-right font-semibold">{{ number }}.</p>
+        <div
+            class="flex items-center gap-1"
+            :class="voted ? 'text-orange-500' : ''"
+        >
+            <a
+                href="javascript:void(0)"
+                class="
+                    mt-1
+                    inline-block
+                    p-0.5
+                    py-1
+                    hover:bg-gray-200
+                    transition-colors
+                    rounded
+                "
+                @click="voted = !voted"
+            >
+                <svg
+                    height="12"
+                    width="14"
+                    class="fill-gray-500 stroke-gray-500"
+                    :class="voted ? 'fill-orange-500 stroke-orange-500' : ''"
+                >
+                    <path
+                        d="M7,2 L2,10 L12,10 z"
+                        stroke-width="2"
+                        stroke-linejoin="round"
+                    />
+                </svg>
+            </a>
+            {{ votes }}
+        </div>
+        <div class="leading-tight">
+            <a :href="url">
+                <span class="text-lg font-semibold">
+                    {{ title }}
+                </span>
+                <span class="text-gray-500">({{ url_short }})</span>
+            </a>
+            <p class="text-gray-500">
+                by
+                <NuxtLink :to="`/user/${author}`" class="sub-link">{{
+                    author
+                }}</NuxtLink>
+                {{ time }} |
+                <NuxtLink :to="`/comments/${id}`" class="sub-link">
+                    {{ comments }} comments
+                </NuxtLink>
+            </p>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+    props: {
+        id: String,
+        number: Number,
+        title: String,
+        author: String,
+        time: String,
+        url_short: String,
+        url: String,
+        comments: Number,
+        votes: Number,
+    },
+    data() {
+        return {
+            voted: false,
+        };
+    },
+});
+</script>
+
+<style>
+.sub-link {
+    @apply border-b-2 hover:(text-black border-b-gray-400) transition-colors;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,19 @@
 <template>
     <div class="min-h-screen bg-gray-50">
         <Header />
+        <div class="flex flex-col gap-4 max-w-5xl m-auto mt-4">
+            <Post
+                id="28878991"
+                :number="1"
+                title="Most canadians believe Facebook harms their mental health"
+                author="elorant"
+                time="2 hours ago"
+                url_short="theglobeandmail.com"
+                url="https://www.theglobeandmail.com/canada/article-most-canadians-believe-facebook-harms-their-mental-health-survey-2/"
+                :comments="100"
+                :votes="297"
+            />
+        </div>
     </div>
 </template>
 
