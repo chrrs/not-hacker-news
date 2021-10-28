363ce90937d7b43141becc72b0863fa697423be9
Chris <chirssel@gmail.com>
2021-10-28 18:32:34 +0200

    Add back the page header

---
+++ components/Header.tsx
@@ -0,0 +1,48 @@
+import Link from 'next/link';
+
+const Header: React.FC = () => {
+    return (
+        <div className="bg-white p-2">
+            <div className="flex justify-between max-w-5xl mx-auto">
+                <div className="flex items-center">
+                    <Link href="/" passHref>
+                        <a className="flex items-center gap-2 mr-2">
+                            <span className="inline-block bg-red-500 w-8 h-8 text-white" />
+                            <span className="font-semibold">
+                                <span className="border-b-2 border-black">
+                                    Not
+                                </span>{' '}
+                                Hacker News
+                            </span>
+                        </a>
+                    </Link>
+                    <Link href="/" passHref>
+                        <a className="header-link">New</a>
+                    </Link>
+                    <Link href="/" passHref>
+                        <a className="header-link">Past</a>
+                    </Link>
+                    <Link href="/" passHref>
+                        <a className="header-link">Comments</a>
+                    </Link>
+                    <Link href="/" passHref>
+                        <a className="header-link">Ask</a>
+                    </Link>
+                    <Link href="/" passHref>
+                        <a className="header-link">Show</a>
+                    </Link>
+                    <Link href="/" passHref>
+                        <a className="header-link">Jobs</a>
+                    </Link>
+                </div>
+                <div className="flex items-center">
+                    <Link href="/login" passHref>
+                        <a className="header-link">Login</a>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import { NextPage } from 'next';
+import Header from '../components/Header';
 
 const Home: NextPage = () => {
-    return <div></div>;
+    return (
+        <div className="min-h-screen bg-gray-50">
+            <Header />
+        </div>
+    );
 };
 
 export default Home;
