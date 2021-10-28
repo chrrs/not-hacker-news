3921be8f35b2a34b485ebf327779f6de10732efe
Chris <chirssel@gmail.com>
2021-10-28 19:16:39 +0200

    Add back the login page

---
+++ pages/login.tsx
@@ -0,0 +1,36 @@
+import { NextPage } from 'next';
+import Image from 'next/image';
+import Header from '../components/Header';
+
+const NotFound: NextPage = () => {
+    return (
+        <div className="flex flex-col min-h-screen bg-gray-50">
+            <Header />
+            <div className="flex flex-col gap-4 max-w-md w-full m-auto mt-8">
+                <h1 className="font-semibold text-2xl text-center">Log in</h1>
+                <div className="flex flex-col gap-2">
+                    <button className="login-button bg-blue-600 hover:bg-blue-500 active:bg-blue-700">
+                        <Image
+                            src="/icons/google.svg"
+                            alt="Google Logo"
+                            width={24}
+                            height={24}
+                        />
+                        Continue with Google
+                    </button>
+                    <button className="login-button disabled bg-black">
+                        <Image
+                            src="/icons/github.svg"
+                            alt="GitHub Logo"
+                            width={24}
+                            height={24}
+                        />
+                        Continue with GitHub
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default NotFound;
