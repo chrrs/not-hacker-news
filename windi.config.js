434811144ef11d44af5fb7f8e24e919f3e07e052
Chris <chirssel@gmail.com>
2021-10-31 14:50:56 +0100

    Define the variant order in the WindiCSS config

--- windi.config.js
+++ windi.config.js
@@ -1,4 +1,22 @@
-module.exports = {
+import { defineConfig } from 'windicss/helpers';
+
+export default defineConfig({
+    variantOrder: [
+        'first',
+        'last',
+        'odd',
+        'even',
+        'visited',
+        'checked',
+        'group-hover',
+        'group-focus',
+        'focus-within',
+        'hover',
+        'focus',
+        'focus-visible',
+        'active',
+        'disabled',
+    ],
     extract: {
         include: ['**/*.{tsx,css}'],
         exclude: ['node_modules', '.git', '.next/**/*'],
@@ -8,4 +26,4 @@ module.exports = {
         extend: {},
     },
     plugins: [],
-};
+});
