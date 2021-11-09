dfd38df39923e8d9aac827066d1193ced0cb32af
Chris <chirssel@gmail.com>
2021-11-09 20:54:43 +0100

    Add posted date to the Post GraphQL type

--- lib/graphql/server.ts
+++ lib/graphql/server.ts
@@ -3,23 +3,40 @@ import { ApolloServer, gql } from 'apollo-server-micro';
 import { IResolvers } from '@graphql-tools/utils';
 import { getDb } from '../../lib/mongo';
 import { Db } from 'mongodb';
+import { GraphQLScalarType, Kind } from 'graphql';
 
 const typeDefs = gql`
+    scalar Date
+
     type Post {
         title: String!
         url: String
+        posted: Date!
     }
 
     type Query {
-        posts: [Post]
+        posts: [Post!]
     }
 `;
 
 const resolvers: IResolvers = {
+    Date: new GraphQLScalarType({
+        name: 'Date',
+        parseValue(value: string) {
+            return new Date(value);
+        },
+        serialize(value: Date) {
+            return value.toISOString();
+        },
+    }),
     Query: {
         posts: async (_parent, _args, _context: { db: Db }, _info) =>
             (await _context.db.collection('posts').find().toArray()).map(
-                (post) => ({ title: post.title, url: post.url })
+                (post) => ({
+                    title: post.title,
+                    url: post.url,
+                    posted: post.posted,
+                })
             ),
     },
 };
